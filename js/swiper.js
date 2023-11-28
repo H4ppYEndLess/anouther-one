2d93276047ee95f05f382868dbb116bc91126f4a
H4ppy <95917527+H4ppYEndLess@users.noreply.github.com>
2023-11-28 22:44:30 +0200

    top vidos
    https://www.youtube.com/watch?v=75Mw8r5gW8E&ab_channel=ElectricCallboy

---
+++ js/swiper.js
@@ -0,0 +1,42 @@
+const swiper = new Swiper('.products', {
+  navigation:{
+    nextEl:'.products__next',
+    prevEl:'.products__prev'
+  },
+      loop:true,
+      slidesPerView:4,
+      spaceBetween: 19,
+      slideToClickedSlide:true,
+      breakpoints: {
+        // when window width is >= 320px
+        1024:{
+          slidesPerView:4
+        },
+        768:{
+          slidesPerView:2,
+        },
+        0:{
+          slidesPerView:1,
+        },
+      }
+});
+
+const slider = new Swiper('.stroke__slider', {
+  // navigation:{
+  //   nextEl:'.slider__next',
+  //   prevEl:'.slider__prev'
+  // },
+      speed:1000,
+      centeredSlides:true,
+      loop:true,
+      slidesPerView:1,
+      spaceBetween: 40,
+      slideToClickedSlide:true,
+      allowTouchMove:false,
+      autoplay:{
+        delay:1000
+      },
+      breakpoints: {
+        // when window width is >= 320px
+      }
+});
