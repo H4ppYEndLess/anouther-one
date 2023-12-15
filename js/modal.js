83970d22d2ca720dbce6f0740d6f9789f8b42d68
H4ppy <95917527+H4ppYEndLess@users.noreply.github.com>
2023-12-15 13:14:32 +0200

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -36,7 +36,7 @@
         var subscribe__name = document.getElementById("name").value;
         var subscribe__phone = document.getElementById("phone").value;
     
-        var subscribe = `New subscriber <b>${subscribe__name}</b>Email:<b>${subscribe__email}</b> Name: <b>${subscribe__name}</b>`
+        var subscribe = `New subscriber <b>${subscribe__name}</b> Email:<b>${subscribe__email}</b> Name: <b>${subscribe__name}</b>`
     
         var token = "6270391808:AAEeDST2kRZacIxzisJz-GBovp8C8uRHcY0";
         var chat_id = '841530321';
